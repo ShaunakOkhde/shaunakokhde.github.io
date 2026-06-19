3f612725db9ee0af8c9b9d05883df10d05b2bca5
Cosmiway Labs <shaunakokhde@gmail.com>
2026-06-19 12:51:42 +0530

    Update script.js

--- music/the-tables-have-turned/script.js
+++ music/the-tables-have-turned/script.js
@@ -1,1 +1,17 @@
+// script.js
+
+const player = document.getElementById("player");
+const statusText = document.getElementById("status");
+
+player.addEventListener("play",()=>{
+statusText.textContent = "Now Playing 🎵";
+});
+
+player.addEventListener("pause",()=>{
+statusText.textContent = "Paused";
+});
+
+player.addEventListener("ended",()=>{
+statusText.textContent = "Thanks for listening ❤️";
+});
 
